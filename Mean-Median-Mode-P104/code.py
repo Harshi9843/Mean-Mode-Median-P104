f44f77d95a295bca28210619c20991161971ab68
Harshi <72039632+Harshi9843@users.noreply.github.com>
2021-07-30 15:22:50 +0100

    Add files via upload

---
+++ Mean-Median-Mode-P104/code.py
@@ -0,0 +1,97 @@
+import csv
+from collections import Counter
+
+with open("Height-Weight.csv", newline = '') as f:
+	reader = csv.reader(f)
+	file_data = list(reader)
+
+file_data.pop(0)
+new_data = []
+
+for index, height, weight in file_data:
+	tWeight = 0 
+	for i in weight:
+		tWeight += float(weight)
+	
+	weightList = weight.sort()
+	new_data = append(float(weightList))
+
+n = len(new_data)
+total = 0 
+
+for x in new_data:
+	total += x 
+
+mean = total/n
+
+print("Mean is: "+str(mean))
+
+n = len(new_data)
+new_data.sort()
+
+if n % 2 == 0:
+	median1 = float(new_data[n//2])
+	median2 = float(new_data[n//2-1])
+	median = (median+median2)/2
+else:
+	median = new_data[n//2]
+	print(n)
+
+print("Median is: "+str(median))
+
+
+data = Counter(new_data)
+mode_data_for_range = {
+	"75-85": 0,
+	"85-95": 0,
+	"95-105": 0,
+	"105-115": 0,
+	"115-125": 0,
+	"125-135": 0,
+	"135-145": 0,
+	"145-155": 0,
+	"155-165": 0,
+	"165-175": 0
+}
+
+for weight, occurence in data.items():
+	if 75<float(weight)<85:
+		mode_data_for_range["75-85"] += occurence
+
+	elif 85<float(weight)<95:
+		mode_data_for_range["85-95"] += occurence
+
+	elif 95<float(weight)<105:
+		mode_data_for_range["95-105"] += occurence
+
+	elif 105<float(weight)<115:
+		mode_data_for_range["105-115"] += occurence
+
+	elif 115<float(weight)<125:
+		mode_data_for_range["115-125"] += occurence
+
+	elif 125<float(weight)<135:
+		mode_data_for_range["125-135"] += occurence
+
+	elif 135<float(weight)<145:
+		mode_data_for_range["135-145"] += occurence
+
+	elif 145<float(weight)<155:
+		mode_data_for_range["145-155"] += occurence
+
+	elif 155<float(weight)<165:
+		mode_data_for_range["155-165"] += occurence
+
+	elif 165<float(weight)<175:
+		mode_data_for_range["165-175"] += occurence
+
+mode_range, mode_occurence = 0, 0
+
+for range, occurence in mode_data_for_range.items():
+	if occurence > mode_occurence:
+		mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+
+mode = float((mode_range[0]+mode_range[1])/2)
+
+print(f"Mode is ->{mode:2f} ") 
+ 
